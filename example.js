7fe35e8d876e5c335fc68f5c01b44cd3803ef1ee
Rosano Coutinho <github.com@xeari.com>
2026-01-27 08:24:52 +0100

    spec unauthorized

--- example.js
+++ example.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const main = require('./main.js');
 
+const port = process.env.PORT || 3000;
+express()
+  .use(main.default.handle)
+  .listen(port, () => console.info(`> Running on port ` + port));
--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const mod = {
 
+	handle (req, res, next) {
+		const root = req.protocol + '://' + req.get('host');
+
+		if (req.url.toLowerCase().match('/.well-known/webfinger'))
+			return res.json({
+				links: [{
+					rel: 'remotestorage',
+					href: root + '/storage',
+				}],
+			});
+
+		return res.status(401).send('Unauthorized');
+
+		return next();
+	},
+
+};
+
+export default mod;
