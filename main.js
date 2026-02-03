1f8db5bf94a29c588deae6705d0875ae10632653
Rosano Coutinho <github.com@xeari.com>
2026-02-03 09:02:40 +0100

    update spec version

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const mod = {
 				links: [{
 					rel: 'remotestorage',
 					href: `${ req.protocol }://${ req.get('host') }/${ prefix }/me`,
-					type: 'draft-dejong-remotestorage-06',
+					type: 'draft-dejong-remotestorage-07',
 				}],
 			});
 
