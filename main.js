54579a9eef0a42720a2336939f39a93db51ff48b
Rosano <git@xeari.com>
2026-03-18 10:36:19 +0100

    sync

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import fs from 'fs';
-
 const mod = {
 
 	util: {
@@ -28,6 +26,61 @@ const mod = {
 			const string = e.trim();
 			const quote = '"';
 			return string.startsWith(quote) && string.endsWith(quote) ? string.slice(1, -1) : string;
+		},
+
+		sveltekit: (middleware, path) => async ({ event, resolve }) => {
+			if (path && !event.url.pathname.startsWith(path))
+				return resolve(event);
+
+			const [protocol, host] = [event.url.protocol.replace(/\:$/, ''), event.url.host];
+			const req = {
+				url: `${ event.url.pathname }${ event.url.search }`,
+				protocol,
+				query: event.url.search,
+				method: event.request.method,
+				headers: Object.fromEntries(event.request.headers),
+				get: key => ({
+					host,
+				}[key]),
+			};
+
+			if (path)
+				req.url = req.url.replace(new RegExp(`^${ path.replaceAll('/', '\\/') }`), '');
+
+			if (req.method === 'PUT' && !event.request.__body)
+				event.request.__body = await (function () {
+					if (event.request.headers.get('content-type').startsWith('application/json'))
+						return event.request.json();
+
+					if (event.request.headers.get('content-type').startsWith('text/'))
+						return event.request.text();
+					
+					return event.request.arrayBuffer();
+				})();
+
+			if (req.method === 'PUT')
+				req.body = event.request.__body;
+
+			event.__headers = event.__headers || {};
+
+			const res = {
+				set: obj => (Object.keys(obj).forEach(key => event.__headers[key] = obj[key]), res),
+
+				status: code => (res._status = code, res),
+				json: obj => res.send(JSON.stringify(obj)),
+				send: body => (res.body = body, res.end()),
+				end: () => new Response(res.body, {
+					status: res._status || 200,
+					headers: event.__headers,
+				}),
+			};
+
+			return middleware(req, res, err => {
+				if (err)
+					throw err;
+
+				return resolve(event);
+			});
 		},
 
 	},
@@ -65,16 +118,16 @@ const mod = {
 				rel: 'http://tools.ietf.org/id/draft-dejong-remotestorage',
 				href: `${ base }${ await storagePath(handle) }`,
 				properties: {
-					'http://remotestorage.io/spec/version': 'draft-dejong-remotestorage-11',
+					'http://remotestorage.io/spec/version': 'draft-dejong-remotestorage-13',
 					'http://tools.ietf.org/html/rfc6749#section-4.2': `${ base }${ authPath }`,
 				},
 			}],
 		});
 	},
 
-	storage: ({ hold, getScope }) => async (req, res, next) => {
+	remotestorage: ({ hold, getScope }) => async (req, res, next) => {
 		// console.info(req.method, req.url);
-		const [handle, isPublicFolder, path] = mod.util.parsePathname(req.url);
+		const [handle, isPublicFolder, relative] = mod.util.parsePathname(req.url);
 		const token = mod.util.parseToken(req.headers.authorization);
 
 		if (!isPublicFolder && !token)
@@ -82,84 +135,109 @@ const mod = {
 
 		const isFolderRequest = req.url.endsWith('/');
 
-		const scope = await getScope(handle, token);
+		const _scope = await getScope(handle, token);
 
-		if (!scope && isPublicFolder && isFolderRequest)
+		if (!_scope && isPublicFolder && isFolderRequest)
 			return res.status(401).end();
 
-		if (!scope && !isPublicFolder)
+		if (!_scope && !isPublicFolder)
 			return res.status(401).send('missing scope');
 
-		const _scope = path === '/' ? '/' : path.match(/^\/([^\/]+)/).pop();
+		const intent = relative === '/' ? '/' : relative.match(/^\/([^\/]+)/).pop();
+		const available = !_scope ? {
+			// if isPublicFolder, we may have no token but still scope available
+		} : mod.util.parseScopes(_scope);
 
-		const scopes = !scope ? {
-			// if isPublicFolder, we may not have a token
-		} : mod.util.parseScopes(scope);
+		if (!isPublicFolder && _scope && !Object.keys(available).includes(intent) && !Object.keys(available).includes('*'))
+			return res.status(401).send('invalid _scope');
 
-		if (!isPublicFolder && scope && !Object.keys(scopes).includes(_scope) && !Object.keys(scopes).includes('*'))
-			return res.status(401).send('invalid scope');
-
-		if (['PUT', 'DELETE'].includes(req.method) && (!scope || !(scopes[_scope] || scopes['*']).includes('w')))
+		if (['PUT', 'DELETE'].includes(req.method) && (!_scope || !(available[intent] || available['*']).includes('w')))
 			return res.status(401).send('invalid access');
 
 		if (req.method === 'PUT' && req.headers['content-range'])
 			return res.status(400).end();
 
-		const __url = `${ isPublicFolder ? '/public' : ''}${ path }`;
-		const target = hold.dataPath(handle, __url);
-		const targetExists = fs.existsSync(target);
-		
-		if (req.method === 'PUT' && targetExists && fs.statSync(target).isDirectory())
+		const target = `${ isPublicFolder ? '/public' : ''}${ relative }`;
+		const exists = await hold.exists({
+			handle,
+			target,
+		});
+
+		const _breadcrumbs = target.split('/').slice(1).reduce((coll, item) => {
+			if (coll.at(-1))
+				item = `${ coll.at(-1) || '' }/${ item }`;
+
+			return coll.concat(item);
+		}, []);
+
+		if (req.method === 'PUT' && await Promise.all(_breadcrumbs.slice(1).map(async target => {
+			const exists = await hold.exists({
+				handle,
+				target,
+			});
+			return {
+				target,
+				exists,
+				isFolder: exists && await hold.isFolder({
+					handle,
+					target,
+				}),
+			};
+		})).then(e => e.filter(e => e.exists && (`/${ e.target }` === target ? e.isFolder : !e.isFolder)).length))
 			return res.status(409).end();
 
-		const ancestors = __url.split('/').slice(0, -1).reduce((coll, item) => {
-			return coll.concat(`${ coll.at(-1) || '' }/${ item }`);
-		}, []).map(e => hold.dataPath(handle, e));
-		
-		if (req.method === 'PUT' && !targetExists)
-			if (ancestors.filter(e => fs.existsSync(e) && fs.statSync(e).isFile()).length)
-				return res.status(409).end();
-
-		const meta = await hold.meta(handle, __url);
+		const meta = await hold.meta({
+			handle,
+			target,
+		});
 
 		if (['PUT', 'DELETE'].includes(req.method) && (
-			!targetExists && req.headers['if-match']
-			|| targetExists && req.headers['if-match'] && mod.util.tidyEtag(req.headers['if-match']) !== meta.ETag
-			|| targetExists && req.headers['if-none-match']
+			!exists && req.headers['if-match']
+			|| exists && req.headers['if-match'] && mod.util.tidyEtag(req.headers['if-match']) !== meta.ETag
+			|| exists && req.headers['if-none-match']
 			))
 			return res.status(412).end();
 
-		if (['HEAD', 'GET', 'DELETE'].includes(req.method) && !targetExists)
-			return isFolderRequest ? res.status(200).json({
+		const listResponse = {
 			'@context': 'http://remotestorage.io/spec/folder-description',
-				items: {},
-			}) : res.status(404).send('Not found');
+			items: {},
+		};
 
-		if (req.method === 'GET' && targetExists && req.headers['if-none-match'] && req.headers['if-none-match'].split(',').map(mod.util.tidyEtag).includes(meta.ETag))
+		if (isFolderRequest)
+			meta['Content-Type'] = 'application/ld+json';
+
+		if (['HEAD', 'GET', 'DELETE'].includes(req.method) && !exists)
+			return isFolderRequest ? res.set(meta).status(200).json(listResponse) : res.status(404).send('Not found');
+
+		if (req.method === 'GET' && exists && req.headers['if-none-match'] && req.headers['if-none-match'].split(',').map(mod.util.tidyEtag).includes(meta.ETag))
 			return res.status(304).end();
+
+		const breadcrumbs = _breadcrumbs.slice(0, -1);
 
 		if (req.method === 'PUT')
 			await hold.put({
 				handle,
-				path: __url,
+				target,
 				data: req.body,
-				ancestors,
+				breadcrumbs,
 				meta: Object.assign(meta, {
 					'Content-Type': req.headers['content-type'],
-					'Last-Modified': new Date().toUTCString(),
 				}),
 			});
 
-		if (req.method === 'DELETE') {
-			await hold.delete(target, ancestors);
-			return res.status(200).end();
-		} 
-
-		if (isFolderRequest)
-			meta['Content-Type'] = 'application/ld+json';
-		
 		meta['ETag'] = `"${ meta['ETag'] }"`;
 
+		if (req.method === 'DELETE') {
+			await hold.delete({
+				handle,
+				target,
+				breadcrumbs,
+			});
+			return res.set({
+				ETag: meta['ETag'],
+			}).status(200).end();
+		}
+		
 		res
 			.set(meta)
 			.status(200);
@@ -168,67 +246,18 @@ const mod = {
 			return res.end();
 
 		if (isFolderRequest)
-			return res.json({
-				'@context': 'http://remotestorage.io/spec/folder-description',
-				items: await hold.folderItems(handle, __url),
-			});
+			return res.json(Object.assign(listResponse, {
+				items: await hold.list({
+					handle,
+					target,
+				}),
+			}));
 
-		return res.send(fs.readFileSync(target, meta['Content-Type'].startsWith('application/json') ? 'utf8' : undefined));
-	},
-
-	sveltekit: (middleware, path) => async ({ event, resolve }) => {
-		if (path && !event.url.pathname.startsWith(path))
-			return resolve(event);
-
-		const [protocol, host] = [event.url.protocol.replace(/\:$/, ''), event.url.host];
-		const req = {
-			url: `${ event.url.pathname }${ event.url.search }`,
-			protocol,
-			query: event.url.search,
-			method: event.request.method,
-			headers: Object.fromEntries(event.request.headers),
-			get: key => ({
-				host,
-			}[key]),
-		};
-
-		if (path)
-			req.url = req.url.replace(new RegExp(`^${ path.replaceAll('/', '\\/') }`), '');
-
-		if (req.method === 'PUT' && !event.request.__body)
-			event.request.__body = await (function () {
-				if (event.request.headers.get('content-type').startsWith('application/json'))
-					return event.request.json();
-
-				if (event.request.headers.get('content-type').startsWith('text/'))
-					return event.request.text();
-				
-				return event.request.arrayBuffer();
-			})();
-
-		if (req.method === 'PUT')
-			req.body = event.request.__body;
-
-		event.__headers = event.__headers || {};
-
-		const res = {
-			set: obj => (Object.keys(obj).forEach(key => event.__headers[key] = obj[key]), res),
-
-			status: code => (res._status = code, res),
-			json: obj => res.send(JSON.stringify(obj)),
-			send: body => (res.body = body, res.end()),
-			end: () => new Response(res.body, {
-				status: res._status || 200,
-				headers: event.__headers,
-			}),
-		};
-
-		return middleware(req, res, err => {
-			if (err)
-				throw err;
-
-			return resolve(event);
-		});
+		return res.send(await hold.get({
+			handle,
+			target,
+			contentType: meta['Content-Type'],
+		}));
 	},
 
 };
